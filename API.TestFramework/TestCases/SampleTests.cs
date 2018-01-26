6a3683d3c82d074c2ee94338e963af74d71cd743
Marc Dacanay <mdacanay@gmail.com>
2018-01-26 22:46:50 +1100

    Add Tests for PUT and DELETE

--- API.TestFramework/TestCases/SampleTests.cs
+++ API.TestFramework/TestCases/SampleTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using NUnit.Framework;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 
@@ -19,22 +20,38 @@ namespace API.TestFramework
             {
                 BaseAddress = new Uri(@"https://jsonplaceholder.typicode.com")
             };
-        }        
-
-        [Test]
-        public void SubmitBlogPost()
-        {
-            _client.Method = Method.POST;
-            _client.Content = new StringContent("{\"userId\": 1, \"title\": \"my awesome post\", \"body\": \"this is my awesome post!\"}", Encoding.UTF8, "application/json");
-            var response = _client.Request($"/posts");
         }
 
         [Test]
-        public void GetBlogPosts()
+        public void GetAllBlogPosts()
         {
             var response = _client.Request($"/posts");
+            int blogCount = Enumerable.Count(response);
+            blogCount.Should().BeGreaterOrEqualTo(1);
             string userId = response[0].userId.ToString();
             userId.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void CreateBlogPost()
+        {           
+            var blogTitle = "my awesome post";
+            _client.Method = Method.POST;
+            _client.Content = new StringContent($"{{\"userId\": 1, \"title\": \"{blogTitle}\", \"body\": \"this is my awesome post!\"}}", Encoding.UTF8, "application/json");
+            var response = _client.Request($"/posts");
+            string blogId = response.id.ToString();
+            blogId.Should().NotBeEmpty();            
+        }
+
+        [Test]
+        public void UpdateBlogPost()
+        {
+            var blogTitle = "my awesome post";
+            _client.Method = Method.PUT;
+            _client.Content = new StringContent($"{{\"userId\": 1, \"title\": \"{blogTitle}\", \"body\": \"this is my awesome post!\"}}", Encoding.UTF8, "application/json");
+            var response = _client.Request($"/posts/1");
+            string title = response.title.ToString();
+            title.Should().Be(blogTitle);
         }
 
         [Test]
@@ -44,6 +61,15 @@ namespace API.TestFramework
             var response = _client.Request($"/posts/{expectedId}");
             string actualId = response.id.ToString();
             actualId.Should().Be(expectedId);
+        }
+
+        [Test]
+        public void DeleteBlogPost()
+        { 
+            _client.Method = Method.DELETE;
+            var response = _client.Request($"/posts/1");
+            string blogPost = response.ToString();
+            blogPost.Should().Be("{}");
         }
     }
 }
