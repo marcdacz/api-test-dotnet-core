5f8478911a2fa35e19ab65d2aa95a6d61de1f211
Marc Dacanay <mdacanay@gmail.com>
2018-01-27 14:56:27 +1100

    Add BastTestFixture and use of TestSettings.json through ConfigurationBuilder

---
+++ API.TestFramework/Environment/BaseTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace API.TestFramework.Environment
+{
+    public class BaseTestFixture
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public Uri BaseAddress { get; set; }
+        public Client Client { get; set; }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("testsettings.json");
+
+            Configuration = builder.Build();
+            BaseAddress = new Uri (Configuration.GetSection("TestAPIUrl").Value);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Client = new Client
+            {
+                BaseAddress = BaseAddress
+            };
+        }
+    }
+}
--- API.TestFramework/TestCases/SampleTests.cs
+++ API.TestFramework/TestCases/SampleTests.cs
@@ -7,23 +7,12 @@ using System.Linq;
 namespace API.TestFramework
 {
     [TestFixture]
-    public class SampleTests
+    public class SampleTests : BaseTestFixture
     {
-        private Client _client;
-
-        [SetUp]
-        public void Setup()
-        {
-            _client = new Client
-            {
-                BaseAddress = new Uri(@"https://jsonplaceholder.typicode.com")
-            };
-        }
-
         [Test]
         public void GetAllBlogPosts()
         {
-            var response = _client.Request($"/posts");
+            var response = Client.Request($"/posts");
             int blogCount = Enumerable.Count(response);
             blogCount.Should().BeGreaterOrEqualTo(1);
             string userId = response[0].userId.ToString();
@@ -32,11 +21,11 @@ namespace API.TestFramework
 
         [Test]
         public void CreateBlogPost()
-        {           
-            var data = new { userId = 1, title = "my awesome post", body = "this is my awesome post!" };           
-            _client.Content = data.AsJson();
-            _client.Method = Method.POST;
-            var response = _client.Request($"/posts");
+        {
+            var data = new { userId = 1, title = "my awesome post", body = "this is my awesome post!" };
+            Client.Content = data.AsJson();
+            Client.Method = Method.POST;
+            var response = Client.Request($"/posts");
             string blogId = response.id.ToString();
             blogId.Should().NotBeEmpty();
         }
@@ -47,9 +36,9 @@ namespace API.TestFramework
             var blogTitle = "my awesome updated post";
             var blogDetails = "this is my awesome updated post!";
             var data = new { userId = 1, title = blogTitle, body = blogDetails };
-            _client.Content = data.AsJson();
-            _client.Method = Method.PUT;
-            var response = _client.Request($"/posts/1");
+            Client.Content = data.AsJson();
+            Client.Method = Method.PUT;
+            var response = Client.Request($"/posts/1");
             string title = response.title.ToString();
             title.Should().Be(blogTitle);
             string details = response.body.ToString();
@@ -60,16 +49,16 @@ namespace API.TestFramework
         public void GetBlogPostById()
         {
             string expectedId = "1";
-            var response = _client.Request($"/posts/{expectedId}");
+            var response = Client.Request($"/posts/{expectedId}");
             string actualId = response.id.ToString();
             actualId.Should().Be(expectedId);
         }
 
         [Test]
         public void DeleteBlogPost()
-        { 
-            _client.Method = Method.DELETE;
-            var response = _client.Request($"/posts/1");
+        {
+            Client.Method = Method.DELETE;
+            var response = Client.Request($"/posts/1");
             string blogPost = response.ToString();
             blogPost.Should().Be("{}");
         }
